1e04821cad18f41cd8759d7cff1c43b8afd0ddda
fcharoensudjai <fasai.c9@gmail.com>
2024-10-16 22:14:31 +0100

    mantou's 3rd dessert

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/portfolio',  // Replace with your GitHub repo name
-    assetPrefix: '/portfolio/',  // Same as above
 };
 
 export default nextConfig;
