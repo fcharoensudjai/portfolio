76f04464bb3385e8ccbd125213682df90facc409
fcharoensudjai <fasai.c9@gmail.com>
2024-10-16 23:17:47 +0100

    mantou's last dessert

--- src/app/recents/[id]/artworks.ts
+++ src/app/recents/[id]/artworks.ts
@@ -10,7 +10,7 @@ interface artworkData {
 export const artworks: artworkData[] = [
     {
         id:1,
-        banner: "/images/raiden-shogun/raiden-banner.png",
+        banner: "/portfolio/images/raiden-shogun/raiden-banner.png",
         alt: "drawing of raiden shogun",
         title: "raiden shogun",
         description: "Raiden Shogun has long been my favourite character in Genshin Impact, and I wanted to capture her energy as an Archon. I did this using a composition that was inspired by the League of Legends splash art styles, as they are often dynamic, atmospheric and incredibly epic. Taking on one of the biggest digital canvases I've ever worked on was both exciting and challenging - the compressed PNG in the end ended up being 81MB and my iPad crashed a couple times! I aimed for a high level of detail, but this had to be balanced with maintaining a good overall composition and keeping a solid bigger picture.",
@@ -18,7 +18,7 @@ export const artworks: artworkData[] = [
 
     {
         id:2,
-        banner: "/images/raven/raven-banner.png",
+        banner: "/portfolio/images/raven/raven-banner.png",
         alt: "drawing of raven",
         title: "raven",
         description: "I joined a \"Draw This In Your Style Challenge\" for Raven, a character designed by another artist on Instagram. This project was a great learning experience, especially for drawing metal, which I drew great inspiration from WLOP's works. This piece also allowed me to work on drawing backgrounds further, especially after drawing Raiden Shogun. I was very happy to be featured on the front of the artist's post at the end of the challenge!",
