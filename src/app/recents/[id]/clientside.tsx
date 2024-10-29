07d89f45016a19fd4f8938f13e335851408dc00e
fcharoensudjai <fasai.c9@gmail.com>
2024-10-29 00:37:29 +0000

    mantou's midnight snack

--- src/app/recents/[id]/clientside.tsx
+++ src/app/recents/[id]/clientside.tsx
@@ -113,7 +113,7 @@ export default function Recent() {
 
                 <Fader enterDelay={1.2} once={true}>
                     <div className={'flex flex-col space-y-5 px-6 md:px-16 xl:px-20'}>
-                        <div className={`w-full xl:w-[65%] lg:min-h-[285px] flex items-center`}><Textbox
+                        <div className={`w-full 2xl:w-[65%] lg:min-h-[285px] flex items-center`}><Textbox
                             caret={false}><Scramble delay={1500}>{String(artwork.description)}</Scramble></Textbox>
                         </div>
 
