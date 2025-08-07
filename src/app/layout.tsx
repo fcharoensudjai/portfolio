294780c6e272b2198335e5f969f6534109783400
fcharoensudjai <fasai.c9@gmail.com>
2025-08-07 14:43:15 +0100

    updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
     title: "fuzzch | portfolio",
     description: "welcome to my portfolio!",
     icons: {
-        icon: "/portfolio/icons/light/logo.svg" // logo for thumbnail
+        icon: "/portfolio/icons/dark/logodark.svg" // logo for thumbnail
     },
 };
 
