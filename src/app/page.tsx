cfb39e5bde27ffa10f9f615f355cf50fbff6e6de
fcharoensudjai <fasai.c9@gmail.com>
2026-03-28 18:26:13 +0000

    try different delay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,7 @@ export default function Home() {
           setTimeout(() => attemptScroll(retries - 1), 200);
         }
       };
-      setTimeout(() => attemptScroll(), 1500);
+      setTimeout(() => attemptScroll(), 1200);
     }
   }, []);
 
